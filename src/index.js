c4a835a30b04be72c6cabc9b080d1e724f025d97
Aakashkalla <aakashpradeepkalla@gmail.com>
2025-08-02 17:06:33 +0530

    Created 2 models and production level backend done

--- src/index.js
+++ src/index.js
@@ -5,7 +5,14 @@ dotenv.config({
     path: './.env'
 })
 
-connectDB()
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`)
+    })
+}
+).catch((error)=>{
+    console.log("MONGODB CONNECTION FAILED!!! ", error)
+})
 /*
 (async ()=>{
     try {
